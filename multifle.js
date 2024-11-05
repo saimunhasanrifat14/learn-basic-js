9f359c4b11e8a4199a75a98b28b6ce682d3e591e
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-05 15:15:50 +0600

    learn multifle condition

--- multifle.js
+++ multifle.js
@@ -2,7 +2,7 @@ const salary = 25000;
 const isBCS = true;
 const height = 61;
 
-if(salary > 20000 && height > 60){
+if(salary > 20000 && height > 60 && isBCS == true){
     console.log('su patro')
 }
 else{
