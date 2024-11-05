583f1e07ed623f563368a5208bbbf18894a2c658
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-05 15:51:08 +0600

    learn data-types

---
+++ data-types.js
@@ -0,0 +1,5 @@
+const message = "Hello"; // string
+const number = 5; // number
+const isstudent = true; //boolean
+const colors = ["red", "blue", "greet"]; // array
+const person = { name: "saimun", age: 19}; // object
---
+++ punction.js
@@ -0,0 +1,14 @@
+// function greet() {
+//     console.log("Saimun")
+// }
+// function greet(name){
+//     console.log("Hello" + name)
+// }
+
+const name = ("Saimun");
+function greet(name){
+    console.log("Hello" + name)
+}
+function name(name){
+
+}
