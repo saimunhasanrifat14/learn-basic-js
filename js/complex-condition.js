31a929d0caf25463f0ec3e3ced743082cf4b7e98
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-10 20:16:47 +0600

    condition prectice

--- js/complex-condition.js
+++ js/complex-condition.js
@@ -1,11 +1,48 @@
-const salary =21001;
-const isBCS = true;
-const height =68;
-const hasCar =false;
+// const salary =25000;
+// const isBCS = true;
+// const height =68;
+// const hasCar =false;
 
-if((salary > 25000 && hasCar == true) || isBCS == true){
-    console.log("tomar 14 gosti raji")
+// if((salary > 25000 && hasCar == true) || isBCS == true){
+//     console.log("su patro")
+// }
+// else{
+//     console.log('ja bag')
+// }
+
+
+// let userinput = prompt("Enter your buying price");
+// let price = +userinput;
+// console.log(price)
+// if (price >= 5000){
+//     alert("Get 50% off!")
+// }else if(price >= 4000){
+//     alert("Get 40% off!")
+// }else if(price >= 3000){
+//     alert("Get 30% off!")
+// }else if(price >= 2000){
+//     alert("Get 20% off!")
+// }else if(price >= 1000){
+//     alert("Get 10% off!")
+// }else(alert("You will pay full payment"))
+
+
+// let userinput = prompt("Enter your number ")
+// let number = +userinput;
+// console.log(number)
+// if ( number % 2 == 0) {
+//     alert("It is an even number")
+// }else(
+//     alert("It is an odd number")
+// )
+
+
+let userinput = prompt("Enter your age")
+let age = +userinput;
+console.log(age)
+if (age >= 18 && age <= 80){
+    alert("tumi bot dite parba")
 }
-else{
-    console.log('ja bag')
-}
+else(
+    alert("tumi bot dite parba na")
+)
