202fd4c58c6ded5e7764b842c197aaf8a67b9da5
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-15 23:15:49 +0600

    learn do while loops

---
+++ js/do-while-loops.js
@@ -0,0 +1,12 @@
+let title = document.getElementById('title');
+let x = 1;
+
+do{
+    console.log("Count" , x);
+    title.innerHTML += `Number count : ${x} </br>`;
+    x++ 
+}
+while(
+    // x <= 1000
+    false
+)
