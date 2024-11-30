5e02e77ed6e2063954560828aff56a1fb762a63d
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-30 22:21:05 +0600

    learn slice or splice

--- js/array-methods.js
+++ js/array-methods.js
@@ -1,37 +1,50 @@
-// add more item
+//<-------------- add more item --------------->
 // let foodItems = ["potato", "apple", "litchi", "tomato"];
 // foodItems.push("chips", "burger", "paneer", "mengo");
 // console.log(foodItems);
 
 
-// delete last item
+//<-------------- delete last item --------------->
 // let foodItems = ["potato", "apple", "litchi", "tomato"];
 // let deletedItem = foodItems.pop();
 // console.log(foodItems);
 // console.log("Deleted", deletedItem);
 
-// array to string convart
+//<-------------- array to string convart --------------->
 // let foodItems = ["potato", "apple", "litchi", "tomato"];
 // console.log(foodItems);
 // console.log(foodItems.toString());
 // console.log(foodItems);
 
-// add one array with anather array
+//<-------------- add one array with anather array --------------->
 // let mervelHeroes = ["thor", "spiderman", "ironman"];
 // let dcHeroes = ["superman", "batman"];
 // let bdHeroes = ["sakib", "Sharukh"]
 // let heroes = mervelHeroes.concat(dcHeroes, bdHeroes);
 // console.log(heroes)
 
-// add a item on first of array
+//<-------------- add a item on first of array --------------->
 // let marvelHeroes = ["thor", "spiderman", "ironman"];
 // let val = marvelHeroes.unshift("antman");
 // console.log(marvelHeroes);
 
 
-//delet the first item 
+//<--------------delet the first item--------------->
 // let marvelHeroes = ["thor", "spiderman", "ironman"];
 // let val = marvelHeroes.shift();
 // console.log("deleted itme", val);
+// console.log(marvelHeroes);
+
+
+//<--------------selacte the item what i want--------------->
+// let marvelHeroes = ["thor", "spiderman", "ironman", "antman", "Dr. Dtrang"];
+// console.log(marvelHeroes);
+// console.log(marvelHeroes.slice(3 , 5));
+
+
+//<--------------add element delete element replace element--------------->
+// let marvelHeroes = ["thor", "spiderman", "ironman", "antman", "Dr. Dtrang"];
+// marvelHeroes.splice(2 , 1, "Krish");
+// marvelHeroes.splice(2 , 0, "Krish");
 // console.log(marvelHeroes);
 
