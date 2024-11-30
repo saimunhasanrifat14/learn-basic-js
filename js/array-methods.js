41dd3e1174c25b054949585618ff75638e6e92cb
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-11-30 15:35:52 +0600

    learn concat

---
+++ js/array-methods.js
@@ -0,0 +1,24 @@
+// add more item
+// let foodItems = ["potato", "apple", "litchi", "tomato"];
+// foodItems.push("chips", "burger", "paneer", "mengo");
+// console.log(foodItems);
+
+
+// delete last item
+// let foodItems = ["potato", "apple", "litchi", "tomato"];
+// let deletedItem = foodItems.pop();
+// console.log(foodItems);
+// console.log("Deleted", deletedItem);
+
+// array to string convart
+// let foodItems = ["potato", "apple", "litchi", "tomato"];
+// console.log(foodItems);
+// console.log(foodItems.toString());
+// console.log(foodItems);
+
+// add one array with anather array
+// let mervelHeroes = ["thor", "spiderman", "ironman"];
+// let dcHeroes = ["superman", "batman"];
+// let bdHeroes = ["sakib", "Sharukh"]
+// let heroes = mervelHeroes.concat(dcHeroes, bdHeroes);
+// console.log(heroes)
