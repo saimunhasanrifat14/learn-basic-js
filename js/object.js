b0365fddb72c3c88d41cfb9cddb0c9029194f40a
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-12-04 22:54:42 +0600

    learn object with function loop condition

--- js/object.js
+++ js/object.js
@@ -2,7 +2,63 @@ let myFirstObject = {
     batchName : "mern 2403",
     totalStudent : 23, 
     IsActive : true, 
-    StudentName : ["ismail" , "Rubel" , "sakib"]
+    StudentName : ["ismail" , "Rubel" , "sakib", "Hima", "anuska"],
+    getFemalsStudents : function () {
+        let femaleBlankArr = [];
+        for (let femaleStudant of this.StudentName) {
+            if(
+                femaleStudant == "Hima"|| femaleStudant == "anuska"
+            ){
+                femaleBlankArr.push(femaleStudant);
+            }
+        }
+        return femaleBlankArr;
+        
+    },
+    getmalsStudents : function () {
+        let maleBlankArr = [];
+        for (let maleStudant of this.StudentName) {
+            if(
+                maleStudant == "ismail"|| maleStudant == "Rubel"
+            ){
+                maleBlankArr.push(maleStudant);
+            }
+        }
+        console.log(maleBlankArr);
+    },
+    mentor : {
+        mentor : "Taufik islam", 
+        Department : "Web and software", 
+        designation : "Faculty Member & Mern Stack Developer", 
+        mentorList : ["Shohan", "Tahsan", "wasim", "Rajibur", "Shohel"], 
+        DepartmentHead : {
+            HodName : "SoHan Hossain eva", 
+            experience : 7,
+            EducationalBackground: "BSC in CSE",
 
+        }
+    }, 
+    DigitalMerketing : {
+        teamMember : 45,
+        teamMemberName : ["shariar", "tanjim", "Akram", "Osman"],
+        DigitalMerketingHeadInfo : {
+            DHODHead : "Shovo",
+            totalExperience : 8,
+            getInfoOfdmHead : function (a, b) {
+                console.log(this);
+            }
+        }
+    }
 };
-console.log(myFirstObject);
+
+let returnItem = myFirstObject.getFemalsStudents();
+
+
+// make a object with the help of constructor
+
+function makeFunction(name, total) {
+    this.name = name;
+    this.total = total;
+}
+const myobj = new makeFunction("one", "two")
+console.log(myobj);
