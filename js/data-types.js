1f26080eff64c8c5179ef38e784317286800a1ec
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-17 15:29:33 +0600

    learn data types

--- js/data-types.js
+++ js/data-types.js
@@ -3,3 +3,7 @@ const number = 5; // number
 const isstudent = true; //boolean
 const colors = ["red", "blue", "greet"]; // array
 const person = { name: "saimun", age: 19}; // object
+
+console.log(typeof null); // "object" (Bug in JavaScript)
+console.log(null === undefined); // false
+console.log(null == undefined);  // true (Loose Comparison)
