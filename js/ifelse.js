0c2f0bcb3486a3567fc9eaad1230606de9605c26
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-17 16:43:43 +0600

    prectice conditional stentment

--- js/ifelse.js
+++ js/ifelse.js
@@ -41,3 +41,73 @@
 // else{
 //     console.log("vitore dukte diba na")
 // }
+
+// let drinksPrice = 500;
+// if (drinksPrice >= 500) {
+//   console.log("Free cake");
+// } else {
+//   console.log("coke 30tk");
+// }
+
+//BMI Calculator and Health Category
+// let weight = 60;
+// let height = 1.55448;
+
+// let bmi = weight / (height * height);
+// console.log(`Your BMI is ${bmi}`);
+
+// if (bmi < 18.5) {
+//   console.log("You are underweight");
+// } else if (bmi >= 18.5 && bmi <= 24.9) {
+//   console.log("You are normal");
+// } else if (bmi >= 25 && bmi <= 29.9) {
+//   console.log("you are overweight");
+// } else {
+//   console.log("You are abese");
+// }
+
+// Grade Calculator
+// let userGrade = 82;
+
+// if(userGrade < 100 && userGrade > 90){
+//     console.log("your grade Golden A+");
+// }else if(userGrade < 90 && userGrade >= 80 ){
+//     console.log("your grade A+");
+// }else if(userGrade <= 80 && userGrade > 69){
+//     console.log("your grade A");
+// }else if(userGrade <= 70 && userGrade > 59){
+//     console.log("your grade A-");
+// }else if(userGrade <= 60 && userGrade > 49){
+//     console.log("your grade B");
+// }else if(userGrade <= 50 && userGrade > 33){
+//     console.log("your grade C");
+// }
+
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+
+// let num1 = 10;
+// let num2 = 20;
+// let result = null;
+
+// normal
+// if (num1 < num2) {
+//   result = num1 * 2;
+// }else{
+//     result = num1 + num2;
+// }
+// console.log(result);
+
+// ternary
+// num1 < num2 ? result = num1 * 2 : result = num1 + num2
+// console.log(result);
