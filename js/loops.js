9fe08e79b3b616743d9af4225fd4bb58631c7e44
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-22 00:00:20 +0600

    prectice loop

--- js/loops.js
+++ js/loops.js
@@ -23,6 +23,127 @@
 // console.log(x);
 // }
 
+// let num = 1;
+// let sum = 0;
+// while(num <= 100){
+//     if(num % 2 === 0){
+//         sum = sum + num;
+//     }
+//    num++ 
+// }
+// console.log(sum);
+
+
+// using for loop
+// let sum = 0;
+// for(let n = 1; n <= 10; n++){
+//     sum = sum + n;
+// }
+// console.log("total number is", sum);
+
+
+// using while loop
+// let n = 1;
+// let sum = 0;
+// while(n <= 10){
+//     sum = sum + n;
+//     n++;
+// }
+// console.log("total number is", sum);
+
+// let sum = 0;
+// for(let n = 10; n >= 0; n--){
+//     console.log(n);
+//     sum = sum + n;    
+// }
+// console.log("total number is", sum);
+
+
+// let n = 10;
+// let sum = 0;
+// while(n >= 0){
+//     sum = sum + n;
+//     console.log(n);
+//     n--;
+// }
+// console.log("total number is", sum);
+
+
+
+// for(let n = 1; n <= 20; n++){
+//     if(n % 2 === 0){
+//         console.log("odd number", n);
+//     }
+// }
+// for(let n = 1; n <= 20; n++){
+//     if(n % 2 !== 1){
+//         console.log("odd number", n);
+//     }
+// }
+// for(let n = 0; n <= 20; n += 2){
+//     console.log(n);
+// }
+
+
+// for(let n = 0; n <= 100; n++){
+//     if(n % 5 === 0){
+//         console.log(n);
+//     }
+// }
+
+// let sum = 0;
+// for(let n = 0; n <= 200; n++){
+//     if(n % 5 === 0){
+//         console.log(n);
+//         sum = sum + n;
+//     }
+// }
+// console.log("sum number is ",sum);
+
+// let sum = 0;
+// for(let n = 0; n <= 200; n++){
+//     console.log(n);
+//     sum = sum + n;
+//     if(sum >= 100){
+//         break;
+//     }
+// }
+// console.log("sum number is ",sum);
+
+for(let n = 0; n <= 100; n++){
+    console.log(n);
+}
+
+// set break point
+// for(let n = 0; n <= 100; n++){
+//     console.log(n);
+//     if(n === 10){
+//         break;
+//     }  
+// }
+
+// set continue
+// for(let n = 0; n <= 100; n++){
+//     if(n % 2 === 1){
+//         continue;
+//     }  
+//     console.log(n);
+// }
+
+
+// do while loop 
+// let n = 10;
+// do{
+//     console.log(n);
+//     n++
+// }while(n < 5)
+
+
+// let numbers = [13, 42,53, 64 ,35, 63,24,53];
+// for(let number of numbers){
+//     console.log(number); 
+// }
+
 // let i = 0;
 // while (i < 50) {
 //     console.log(i);
